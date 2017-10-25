bee1e72a048d534b47d1e67ad3e860258e0e3695
gnanesh <gnaneshkunal@outlook.com>
2017-10-25 19:26:54 +0530

    add search reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,7 +1,9 @@
 import * as redux from 'redux';
+import searchReducer from './search';
 
 const rootReducer: redux.Reducer<any> = redux.combineReducers({
-  state: (state = {}) => state
+  // state: (state = {}) => state,
+  search: searchReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/search.ts
@@ -0,0 +1,17 @@
+import {
+    SEARCH_BOOK
+} from '../actions/types';
+
+interface IActionProps {
+    type: string,
+    payload: any
+}
+
+export default function(state = {}, action: IActionProps) {
+    switch(action.type) {
+        case SEARCH_BOOK:
+            return { ...state, data: action.payload };
+    }
+    
+    return state;
+}
