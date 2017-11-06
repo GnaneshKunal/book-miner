4e68f601b4a97fd551598154049b3c7862b781c5
gnanesh <gnaneshkunal@outlook.com>
2017-11-06 23:12:59 +0530

    add reviewer rating interface prop

--- lib/model.ts
+++ lib/model.ts
@@ -8,9 +8,9 @@ export interface IBookReview extends mongoose.Document {
     ratingsCount: Number;
     reviewsCount: Number;
     bookID: Number;
-
     reviewerName: String;
     review: String;
+    reviewerRatings: string
 }
 
 const BookReviewSchema: mongoose.Schema = new mongoose.Schema({
