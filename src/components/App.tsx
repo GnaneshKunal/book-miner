3a7b50a86cf662d2b1cadaa77b1e609474122299
gnanesh <gnaneshkunal@outlook.com>
2017-11-06 23:13:58 +0530

    add header & book view component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Main from './Main';
+import BookView from './BookView';
+import Header from './Header';
 
 const App = (): JSX.Element  => (
     <div>
-        {/* Header Here */}
+         <Header />
         <main>
             <Switch>
                 <Route exact path='/' component={Main} />
+                <Route exact path='/book/:id' component={BookView} />
             </Switch>
         </main>
     </div>
